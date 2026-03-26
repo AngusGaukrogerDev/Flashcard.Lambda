e840ffbf8f80a399fd6f91fb1736f06ca6281c5a
AngusGaukrogerDev <angus@pitahayalabs.com>
2026-03-26 17:21:46 +0000

    Refactor CreateDeckFunction to handle APIGatewayHttpApiV2ProxyRequest and return a structured response. Add Amazon.Lambda.APIGatewayEvents package reference to project file.

--- Flashcards.Functions/CreateDeckFunction.cs
+++ Flashcards.Functions/CreateDeckFunction.cs
@@ -1,11 +1,19 @@
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 
 namespace Flashcards.Functions;
 
 public class CreateDeckFunction
 {
-    public string FunctionHandler(string input, ILambdaContext context)
+    public APIGatewayHttpApiV2ProxyResponse FunctionHandler(
+        APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
     {
-        return input.ToUpper();
+        var body = request.Body; // "Hello World"
+        return new APIGatewayHttpApiV2ProxyResponse
+        {
+            StatusCode = 200,
+            Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } },
+            Body = System.Text.Json.JsonSerializer.Serialize(body?.ToUpper())
+        };
     }
 }
